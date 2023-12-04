4534567183bbac7eb2679c2faacb730439c2b654
shakir <shakkir2227@gmail.com>
2023-12-04 11:35:59 +0530

    one

---
+++ index.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const express = require("express");
+
+mongoose.connect("mongodb://127.0.0.1:27017/user_management_system");
+``
+const app = express();
+
+//for user routes
+const userRoute = require("./routes/userRoute.js");
+app.use("/", userRoute);
+
+//for admin routes
+const adminRoute = require("./routes/adminRoute.js");
+app.use("/admin", adminRoute);
+
+app.listen(3000);
